a4708462d22a96c2c7cb19d792206e2fe29923ec
Jacob Atzen <jatzen@gmail.com>
2015-09-26 13:22:49 +0200

    All tests pass

--- Yatzy/Score.elm
+++ Yatzy/Score.elm
@@ -1,41 +1,107 @@
 module Yatzy.Score where
 
+import Set
+import Debug
+
 upper : Int -> List Int -> Int
 upper n ds =
-  -1
+  let
+      isN x =
+        x == n
+  in
+      ds
+        |> List.filter isN
+        |> List.sum
 
 chance : List Int -> Int
 chance ds =
-  -1
+  List.sum ds
 
 yatzy : List Int -> Int
 yatzy ds =
-  -1
+  let
+      equalToFirst n =
+        Just n == (List.head ds)
+  in
+      if List.all equalToFirst ds
+      then 50
+      else 0
 
 smallStraight : List Int -> Int
 smallStraight ds =
-  -1
+  if List.sort ds == [1,2,3,4,5]
+  then 15
+  else 0
 
 largeStraight : List Int -> Int
 largeStraight ds =
-  -1
+  if List.sort ds == [2,3,4,5,6]
+  then 20
+  else 0
 
 fourOfAKind : List Int -> Int
 fourOfAKind ds =
-  -1
+  someOfAKind 4 ds
 
 threeOfAKind : List Int -> Int
 threeOfAKind ds =
-  -1
+  someOfAKind 3 ds
 
 onePair : List Int -> Int
 onePair ds =
-  -1
+  case List.maximum (pairs ds) of
+    Just x -> 2 * x
+    Nothing -> 0
 
 twoPair : List Int -> Int
 twoPair ds =
-  -1
+  case pairs ds of
+    [x, y] -> (x + y) * 2
+    _ -> 0
 
 fullHouse : List Int -> Int
 fullHouse ds =
-  -1
+  let
+      twoOfAKind = List.head (exactlyCountN 2 ds)
+      threeOfAKind = List.head (exactlyCountN 3 ds)
+  in
+      case (twoOfAKind, threeOfAKind) of
+        (Just x, Just y) -> x * 2 + y * 3
+        _ -> 0
+
+someOfAKind : Int -> List Int -> Int
+someOfAKind count ds =
+  let
+      firstMatching =
+        case List.head (atLeastCountN count ds) of
+          Just n -> n
+          Nothing -> 0
+  in
+      count * firstMatching
+
+uniq : List comparable -> List comparable
+uniq ds =
+  ds
+    |> Set.fromList
+    |> Set.toList
+
+pairs : List Int -> List Int
+pairs ds =
+  ds
+    |> atLeastCountN 2
+    |> List.sort
+    |> uniq
+
+countN : Int -> List Int -> Int
+countN n ds =
+  ds
+    |> List.filter (\x -> x == n)
+    |> List.length
+
+exactlyCountN : Int -> List Int -> List Int
+exactlyCountN count ds =
+  List.filter (\n -> count == countN n ds) ds
+
+atLeastCountN : Int -> List Int -> List Int
+atLeastCountN count ds =
+  List.filter (\n -> count <= countN n ds) ds
