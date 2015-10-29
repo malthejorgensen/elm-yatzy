7aa1808f7fd461d64b4c25f7dbe2cc56946ee8ca
Malthe Jørgensen <malthe.jorgensen@gmail.com>
2015-10-29 14:04:32 +0100

    [Fix] Make tests pass

--- Yatzy/Score.elm
+++ Yatzy/Score.elm
@@ -1,41 +1,86 @@
 module Yatzy.Score where
+import List exposing (sort, head, tail, repeat, drop, take, sum, filter, map, length)
+import String exposing (fromList)
+--import Graphics.Element exposing (show)
+import Debug
+
+eyeCounts ds =
+  let eyeCheck n = (n, filter (\d -> d == n) ds |> length)
+  in
+  map eyeCheck [1..6] |> filter (\(n, c) -> c > 0)
 
 upper : Int -> List Int -> Int
 upper n ds =
-  -1
+  sum <| filter (\d -> d == n) ds
 
 chance : List Int -> Int
 chance ds =
-  -1
+  sum ds
 
 yatzy : List Int -> Int
 yatzy ds =
-  -1
+  case eyeCounts ds of
+    [(p, 5)] -> 50
+    otherwise -> 0
+    --otherwise -> Debug.log (toString <| eyeCounts ds) 0
 
 smallStraight : List Int -> Int
 smallStraight ds =
-  -1
+  let sortedDice = sort ds
+  in
+  if sortedDice == [1,2,3,4,5] then
+    15
+  else
+    0
 
 largeStraight : List Int -> Int
 largeStraight ds =
-  -1
+  let sortedDice = sort ds
+  in
+  if sortedDice == [2,3,4,5,6] then
+    20
+  else
+    0
 
 fourOfAKind : List Int -> Int
 fourOfAKind ds =
-  -1
+  let fours = filter (\(eyes, count) -> count == 4) (eyeCounts ds)
+  in
+  if fours == [] then
+    0
+  else
+    sum <| map (\(p,q) -> p*q) fours
 
 threeOfAKind : List Int -> Int
 threeOfAKind ds =
-  -1
+  let fours = filter (\(eyes, count) -> count == 3) (eyeCounts ds)
+  in
+  if fours == [] then
+    0
+  else
+    sum <| map (\(p,q) -> p*q) fours
 
 onePair : List Int -> Int
 onePair ds =
-  -1
+  let pairs = filter (\(eyes, count) -> count == 2) (eyeCounts ds)
+  in
+  if pairs == [] then
+    0
+  else
+    sum <| map (\(p,q) -> p*q) pairs
 
 twoPair : List Int -> Int
 twoPair ds =
-  -1
+  let pairs = filter (\(eyes, count) -> count == 2) (eyeCounts ds)
+  in
+  if length pairs == 2 then
+    sum <| map (\(p,q) -> p*q) pairs
+  else
+    0
 
 fullHouse : List Int -> Int
 fullHouse ds =
-  -1
+  case eyeCounts ds of
+    [(p, 2), (q, 3)] -> sum ds
+    [(p, 3), (q, 2)] -> sum ds
+    otherwise -> 0
